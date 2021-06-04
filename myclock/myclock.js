68195740e5437c22053fd182045af2da6e45997c
Thomas Erskine <thomas.erskine@gmail.com>
2021-06-03 20:25:41 -0400

    scale font size

--- myclock/myclock.js
+++ myclock/myclock.js
@@ -3,16 +3,15 @@ function showTime() {
     let hour = time.getHours();
     let min = time.getMinutes();
     let sec = time.getSeconds();
-  
+
     hour = hour < 10 ? "0" + hour : hour;
     min = min < 10 ? "0" + min : min;
     sec = sec < 10 ? "0" + sec : sec;
-  
     let currentTime = hour + ":" + min;// + ":" + sec + am_pm;
-  
     document.getElementById("clock")
             .innerHTML = currentTime;
 }
+
 function showDayDisc() {
   var bg = document.getElementById("bg");
   var clk = document.getElementById("clock");
